99953883b3fe686218238c5dd8223797118c22e6
zmuzik <zbynek.muzik@gmail.com>
2015-03-15 11:03:48 +0800

    migration to AS

---
+++ app/src/main/java/zmuzik/slidingpuzzle/Tile.java
@@ -0,0 +1,38 @@
+package zmuzik.slidingpuzzle;
+
+import android.graphics.Bitmap;
+
+public class Tile {
+	Bitmap bitmap;
+	int origX;
+	int origY;
+	int tileNumber;
+
+	public Tile(int x, int y) {
+		origX = x;
+		origY = y;
+	}
+
+	public Tile(int x, int y, Bitmap bitmap, int tileNumber) {
+		origX = x;
+		origY = y;
+		this.bitmap = bitmap;
+		this.tileNumber = tileNumber;
+	}
+
+	public Bitmap getBitmap() {
+		return bitmap;
+	}
+
+	public int getOrigX() {
+		return origX;
+	}
+
+	public int getOrigY() {
+		return origY;
+	}
+
+	public int getTileNumber() {
+		return tileNumber;
+	}
+}
