16856f053c02e52aae4ed1ca68d05fd831b05dc1
Zbynek Muzik <zbynek.muzik@hypersonica.com>
2015-12-30 15:42:21 +0700

    conf file updated by Studio

---
+++ app/src/main/java/zmuzik/slidingpuzzle2/Conf.java
@@ -0,0 +1,26 @@
+package zmuzik.slidingpuzzle2;
+
+import android.content.res.Resources;
+
+import java.util.ResourceBundle;
+
+public class Conf {
+    public static final int DEFAULT_GRID_DIM_SHORT = 4;
+    public static final int DEFAULT_GRID_DIM_LONG = 4;
+    public static final int DEFAULT_GRID_DIMS_POSITION = 4;
+
+    public static final int PAGE_SIZE_LOCAL = 12;
+    public static final int PAGE_SIZE_FLICKR = 12;
+
+    public static final int FLICKR_REQUEST_IMAGES = 48;
+
+    public static final int GRID_COLUMNS_PORTRAIT_HANDHELD = 2;
+    public static final int GRID_COLUMNS_LANDSCAPE_HANDHELD = 3;
+    public static final int GRID_COLUMNS_PORTRAIT_TABLET = 4;
+    public static final int GRID_COLUMNS_LANDSCAPE_TABLET = 6;
+
+    public static final String DCIM_APP_SUBDIR = "SlidingPuzzle";
+    public static final int PIC_QUALITY = 90;
+
+    public static final String FLICKR_API_ROOT = "https://api.flickr.com/services/rest";
+}
