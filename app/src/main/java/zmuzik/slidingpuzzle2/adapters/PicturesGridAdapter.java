e935f2c9f7a4792985edcb6080173f05ef60cfdf
Zbynek Muzik <zbynek.muzik@gmail.com>
2016-07-13 23:35:05 +0200

    more clytics logging

--- app/src/main/java/zmuzik/slidingpuzzle2/adapters/PicturesGridAdapter.java
+++ app/src/main/java/zmuzik/slidingpuzzle2/adapters/PicturesGridAdapter.java
@@ -125,6 +125,7 @@ public class PicturesGridAdapter extends RecyclerView.Adapter<PicturesGridAdapte
         holder.nextTv.setVisibility(View.VISIBLE);
         holder.nextTv.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
+                Crashlytics.log(Log.DEBUG, TAG, "more clicked");
                 showNextPage();
             }
         });
