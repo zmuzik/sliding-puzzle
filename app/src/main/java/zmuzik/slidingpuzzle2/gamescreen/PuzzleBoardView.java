7aec33404c943305c5459831a3926ccc20abb484
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-04-07 11:19:45 +0700

    fix of incorrectly positioned tiles halfway

--- app/src/main/java/zmuzik/slidingpuzzle2/gamescreen/PuzzleBoardView.java
+++ app/src/main/java/zmuzik/slidingpuzzle2/gamescreen/PuzzleBoardView.java
@@ -295,11 +295,14 @@ public class PuzzleBoardView extends ViewGroup {
                         || Math.abs(mMoveDeltaY) > mTileHeight / 2);
                 boolean moveX = mActiveTileY == mBlackTileY;
                 boolean moveY = mActiveTileX == mBlackTileX;
+                mMoveDeltaX = 0;
+                mMoveDeltaY = 0;
 
                 if (makeTheMove) {
                     playTile(mActiveTileX, mActiveTileY);
                     mPuzzleComplete = isPuzzleComplete();
                 }
+                requestLayout();
 
                 for (final TileView tile : tilesToMove) {
                     SpringAnimation anim = null;
@@ -315,9 +318,6 @@ public class PuzzleBoardView extends ViewGroup {
                             .setDampingRatio(SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY);
                     anim.start();
                 }
-                mMoveDeltaX = 0;
-                mMoveDeltaY = 0;
-//                requestLayout();
                 return true;
         }
         return true;
