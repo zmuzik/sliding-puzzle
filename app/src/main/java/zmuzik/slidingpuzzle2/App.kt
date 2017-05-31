722fa49a76a55c28fedd45be4606577e3a056391
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-05-31 21:48:23 +0200

    App class kotlinized

---
+++ app/src/main/java/zmuzik/slidingpuzzle2/App.kt
@@ -0,0 +1,34 @@
+package zmuzik.slidingpuzzle2
+
+import android.app.Application
+import android.content.Context
+
+import com.crashlytics.android.Crashlytics
+
+import io.fabric.sdk.android.Fabric
+import zmuzik.slidingpuzzle2.common.di.AppComponent
+import zmuzik.slidingpuzzle2.common.di.AppModule
+import zmuzik.slidingpuzzle2.common.di.DaggerAppComponent
+
+class App : Application() {
+
+    private val TAG = this.javaClass.simpleName
+
+    lateinit var mAppComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        mAppComponent = DaggerAppComponent.builder().appModule(AppModule(this)).build()
+        Fabric.with(this, Crashlytics())
+    }
+
+    fun getComponent(context: Context): AppComponent {
+        return get(context).mAppComponent
+    }
+
+    companion object {
+        operator fun get(context: Context): App {
+            return context.applicationContext as App
+        }
+    }
+}
