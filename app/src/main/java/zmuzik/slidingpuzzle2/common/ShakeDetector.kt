36d86fc5d53bc12caeec49ed60d301c2264ae5e1
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-06-02 11:51:56 +0200

    ShakeDetector polished

--- app/src/main/java/zmuzik/slidingpuzzle2/common/ShakeDetector.kt
+++ app/src/main/java/zmuzik/slidingpuzzle2/common/ShakeDetector.kt
@@ -6,46 +6,39 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 
-class ShakeDetector(private val mContext: Context) : SensorEventListener {
+class ShakeDetector(private val context: Context) : SensorEventListener {
 
-    companion object {
-        private val SHAKE_FORCE_THRESHOLD = 2f
-        private val SHAKE_SLOP_TIME_MS = 500
-    }
+    private val SHAKE_FORCE_THRESHOLD = 2f
+    private val SHAKE_SLOP_TIME_MS = 500
 
-    private val mSensorManager: SensorManager
-    private val mAccelerometer: Sensor
-    private var mListener: OnShakeListener? = null
-    private var mShakeTimestamp: Long = 0
-    private var mIsRegistered: Boolean = false
-
-    init {
-        mSensorManager = mContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-    }
+    private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    private var listener: OnShakeListener? = null
+    private var shakeTimestamp: Long = 0
+    private var isRegistered: Boolean = false
 
     fun setOnShakeListener(listener: OnShakeListener) {
-        this.mListener = listener
+        this.listener = listener
     }
 
     fun register() {
-        if (!mIsRegistered) {
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI)
-            mIsRegistered = true
+        if (!isRegistered) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI)
+            isRegistered = true
         }
     }
 
     fun unRegister() {
-        if (mIsRegistered) {
-            mSensorManager.unregisterListener(this)
-            mIsRegistered = false
+        if (isRegistered) {
+            sensorManager.unregisterListener(this)
+            isRegistered = false
         }
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
 
     override fun onSensorChanged(event: SensorEvent) {
-        if (mListener != null) {
+        listener?.let {
             val x = event.values[0]
             val y = event.values[1]
             val z = event.values[2]
@@ -58,9 +51,9 @@ class ShakeDetector(private val mContext: Context) : SensorEventListener {
 
             if (totalForce > SHAKE_FORCE_THRESHOLD) {
                 val now = System.currentTimeMillis()
-                if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) return
-                mShakeTimestamp = now
-                mListener!!.onShake()
+                if (shakeTimestamp + SHAKE_SLOP_TIME_MS > now) return
+                shakeTimestamp = now
+                it.onShake()
             }
         }
     }
